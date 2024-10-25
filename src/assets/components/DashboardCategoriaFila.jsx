27f83b496d130acd382b03fa3b5ef6e9f706bab8
FrancoTRS <149124567+FrancoTRS@users.noreply.github.com>
2024-10-25 00:43:31 +0000

    categoria

--- src/assets/components/DashboardCategoriaFila.jsx
+++ src/assets/components/DashboardCategoriaFila.jsx
@@ -2,38 +2,38 @@ import React from "react"
 
 export default function DashboardCategoriaFila(props) {
     
-    const abrirModalEdicion = (id = 1) => {
-        // document.getElementById('modal').classList.remove('hidden');
-        // document.getElementById('tituloModal').textContent = 'Editar Categoría ' + id;
-        document.getElementById('modalEditar').classList.remove('hidden');
-        document.getElementById('tituloModal').textContent = 'Editar Categoría ';
-        props.setId(props.id);
-        props.setNombre(props.nombre);
-        props.setDescripcion(props.descripcion);
-        props.setUrl(props.imagen);
-    };
+    // const abrirModalEdicion = (id = 1) => {
+    //     // document.getElementById('modal').classList.remove('hidden');
+    //     // document.getElementById('tituloModal').textContent = 'Editar Categoría ' + id;
+    //     document.getElementById('modalEditar').classList.remove('hidden');
+    //     document.getElementById('tituloModal').textContent = 'Editar Categoría ';
+    //     props.setId(props.id);
+    //     props.setNombre(props.nombre);
+    //     props.setDescripcion(props.descripcion);
+    //     props.setUrl(props.imagen);
+    // };
 
-    // Simulación de eliminar categoría
-    const eliminarCategoria = (id = 1) => {
-        // if (confirm('¿Estás seguro de eliminar la categoría ' + id + '?')) {
-        //     alert('Categoría ' + id + ' eliminada.');
-        //     // Aquí iría el código para eliminar la categoría en tu sistema
-        // }
-        props.setId(props.id);
-        Swal.fire({
-            title: "¿Está seguro?",
-            text: "No se podrá rehacer una vez eliminado",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Sí, elimínalo"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                props.eliminarDatos(props.id);
-            }
-        });
-    };
+    // // Simulación de eliminar categoría
+    // const eliminarCategoria = (id = 1) => {
+    //     // if (confirm('¿Estás seguro de eliminar la categoría ' + id + '?')) {
+    //     //     alert('Categoría ' + id + ' eliminada.');
+    //     //     // Aquí iría el código para eliminar la categoría en tu sistema
+    //     // }
+    //     props.setId(props.id);
+    //     Swal.fire({
+    //         title: "¿Está seguro?",
+    //         text: "No se podrá rehacer una vez eliminado",
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonColor: "#3085d6",
+    //         cancelButtonColor: "#d33",
+    //         confirmButtonText: "Sí, elimínalo"
+    //     }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             props.eliminarDatos(props.id);
+    //         }
+    //     });
+    // };
 
     return (
         // <tr className="border-b border-b-[#394050]">
@@ -47,7 +47,7 @@ export default function DashboardCategoriaFila(props) {
         // </tr>
         <>
         <tr className="border-b border-b-[#394050]">
-            <td className="text-white font-light py-2 px-4">{props.id}</td>
+            <td className="text-white font-light py-2 px-4">{props.index}</td>
             <td className="text-white font-light py-2 px-4">{props.nombre}</td>
             <td className="text-white font-light text-center py-2 px-4">{props.descripcion}</td>
             <td className="text-white font-light text-center py-2 px-4">
