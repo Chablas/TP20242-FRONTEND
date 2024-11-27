cb5e22e3a1b0d97b3d9d9af0d1446d0631324fab
Christian Alvarez <116127459+Chablas@users.noreply.github.com>
2024-11-27 00:45:20 -0500

    escoger imagen de ordenador en categorias al crear y actualizar, tambien ahora se muestra de acuerdo a lo almacenado en los buckets

--- src/assets/components/StoreProductAll.jsx
+++ src/assets/components/StoreProductAll.jsx
@@ -62,7 +62,7 @@ function StoreProductoPreviewComponent(props) {
         <Link to={`/producto/overview/${props.id}`} className="group">
             <div
                 className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                <img src={`https://compusave-backend.onrender.com${props.imagen}`}
+                <img src={`https://storage.googleapis.com/tallerdeproyectoscompusave/${props.imagen}`}
                     alt="Hand holding black machined steel mechanical pencil with brass tip and top."
                     className="h-full w-full object-cover object-center group-hover:opacity-75" />
             </div>
--- src/assets/components/StoreServicePreview.jsx
+++ src/assets/components/StoreServicePreview.jsx
@@ -51,7 +51,7 @@ function StoreServicePreviewComponent(props) {
     return (
         <div className="group relative">
             <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                <img src={`https://compusave-backend.onrender.com${props.imagen}`} alt={props.nombre} className="h-full w-full object-cover object-center" />
+                <img src={`https://storage.googleapis.com/tallerdeproyectoscompusave/${props.imagen}`} alt={props.nombre} className="h-full w-full object-cover object-center" />
             </div>
             <h3 className="mt-6 text-sm text-gray-500">
                 <Link to={`/StoreServiciosOverview/${props.id}`}>
